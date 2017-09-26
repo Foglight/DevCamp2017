c395c68edaad2020a107bc0c805f0772f516f7ff
Welson <Welson.Yang@software.dell.com>
2017-09-26 14:24:05 +0800

    Host monitoring python agent

---
+++ simplehostmonitor/python/hostMonitoring/hostAgent.py
@@ -0,0 +1,84 @@
+import util
+import socket
+import sys
+import time
+import datetime
+
+class Container:
+    def __init__(self, agentName, startTime, endTime):
+        self.startTime = startTime
+        self.endTime = endTime
+        self.agentName = agentName
+        self.data = []
+
+
+class Host:
+    def __init__(self, hostName):
+        self.typeName = "Host"
+        self.properties = {"hostId":hostName, "name":hostName}
+
+DURATION = 60
+
+print "Please enter the server url, for example: http://10.1.1.1:8080"
+
+prompt = ">"
+url = raw_input(prompt)
+
+print "The url is " + url
+
+print "Try to retrieve token from the url..."
+
+access_token = util.retrieve_token(url)
+
+if access_token is not None:
+    print "Retrieve access token successfully."
+else:
+    print "Failed to retrieve access token, please check log for detail."
+    try:
+    sys.exit(0)
+
+hostName = socket.gethostname()
+while True:
+    host = Host(hostName)
+    hostProperty = host.properties.copy()
+
+    cpu = {};
+    cpu["utilization"] = util.cpu_utilization()
+    cpu["host"] = hostProperty
+
+    memory = {}
+    memory["utilization"] = util.memory_utilization()
+    memory["host"] = hostProperty
+
+    disk = {};
+    disk["diskUtilization"] = util.disk_utilization()
+    disk["host"] = hostProperty
+
+    network = {};
+    network["utilization"] = util.network_utilization()
+    network["host"] = hostProperty
+
+    os = {}
+    os["host"] = hostProperty
+    os["type"] = util.system_platform()
+
+    host.properties["cpus"] = cpu
+    host.properties["memory"] = memory
+    host.properties["network"] = network
+    host.properties["storage"] = disk
+    host.properties["os"] = os
+
+    end = int(round(time.time() * 1000))
+    start = end - (DURATION * 1000)
+    container = Container("Rest_Agent_"+hostName, start, end)
+    container.data.append(host.__dict__)
+
+
+    pushDataResult = util.push_data(url, container, access_token)
+
+    print 'Push data at %s, result is %s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), pushDataResult)
+    time.sleep(DURATION)
+
+
+
+
---
+++ simplehostmonitor/python/hostMonitoring/util.py
@@ -0,0 +1,102 @@
+import urllib
+import urllib2
+import json
+import psutil
+import time
+
+REST_API_PREFIX = "/api/v1"
+
+def retrieve_token(serverURL):
+    loginURL = serverURL + REST_API_PREFIX +"/security/login"
+    body_value = {"authToken": "00ef5e6bac4c0f:844a831861de17e6f88bddee"}
+    body_value = urllib.urlencode(body_value)
+    request = urllib2.Request(loginURL, body_value)
+    result = urllib2.urlopen(request).read()
+    jsonResult = json.loads(result)
+    token = jsonResult["data"]["token"]
+
+    return token;
+
+
+def push_data(serverURL, container, access_token=None):
+    if access_token is None:
+        access_token = retrieve_token(serverURL)
+
+    pushDataURL = serverURL + REST_API_PREFIX + "/topology/pushData"
+    header_dict = {'Access-Token': access_token, 'Content-Type':'application/json'};
+    body_value = json.dumps(container.__dict__)
+    request = urllib2.Request(pushDataURL, headers=header_dict, data=body_value)
+    result = urllib2.urlopen(request).read()
+    jsonResult = json.loads(result)
+    result = jsonResult["data"]
+    return result
+
+
+def cpu_utilization():
+    return psutil.cpu_percent(interval=1)
+
+
+def memory_utilization():
+    return psutil.virtual_memory().percent
+
+
+def disk_utilization():
+    total = 0.0
+    used = 0.0
+    all_disk = psutil.disk_partitions()
+    for disk in all_disk:
+        try:
+            disk_path = disk[1]
+            disk_usage = psutil.disk_usage(disk_path)
+            total += disk_usage[0]
+            used += disk_usage[1]
+        except:
+            continue
+
+    utilization = round(used/total, 2) * 100
+    return utilization
+
+
+def network_utilization():
+    io_conters = psutil.net_io_counters()
+    recvStart = io_conters.bytes_recv
+    sentStart = io_conters.bytes_sent
+
+    time.sleep(1)
+    io_conters = psutil.net_io_counters()
+
+    recvEnd = io_conters.bytes_recv - recvStart
+    sentEnd = io_conters.bytes_sent - sentStart
+    usage = 0.0
+    usage += (recvEnd + sentEnd) * 8.0 / 1000 / 1000
+
+    total = 0.0
+    stats = psutil.net_if_stats()
+    for i in stats:
+        if "Interface" not in str(i):
+            if stats[i][2] is not None:
+                total += stats[i].speed
+    return usage/total * 100
+
+
+def system_platform():
+    cpu_times = psutil.cpu_times()
+    try:
+        cpu_times.iowait
+        return "Linux"
+    except:
+        return "Windows"
+
+
+class Container:
+    def __init__(self, agentName, startTime, endTime):
+        self.startTime = startTime
+        self.endTime = endTime
+        self.agentName = agentName
+        self.data = []
+
+if __name__ == "__main__":
+    # print 'Test Retrieve Token'
+    # print retrieve_token("http://10.154.14.61:8080")
+
+    print system_platform().iowait
